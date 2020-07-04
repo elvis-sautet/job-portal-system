698ba3f5b984ba1e8e656a41bff24d280c43cad8
Elvis Sautet <60774154+elvis-sautet@users.noreply.github.com>
2020-07-04 07:39:42 +0300

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -6,7 +6,7 @@ $password = '';
 try {
     $conn = new PDO("mysql:host=$servername;dbname=placement portal", $username, $password);
     // set the PDO error mode to exception
-    $conn ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $conn ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);//set this to fetch mode object since i will be using objects sometimes while fetching
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
   } catch(PDOException $e) {
